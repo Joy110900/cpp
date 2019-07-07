6dc9d1b9d40888b5cebb018d9efd1168331f25bd
Joy110900 <joy110900@gmail.com>
2019-07-07 21:09:26 +0530

    :tada: initial commit

--- c++_for_programmers/06_functions/2_func_return.cpp
+++ c++_for_programmers/06_functions/2_func_return.cpp
@@ -1,7 +1,31 @@
-#include "2_function_practice.hpp"
+/*
+** Goal: practice writing functions with parameters and return values.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void printEquation(int m1, int m2, int sum, char operation); 
+int sum(int m1, int m2);
 
 int main()
 {
-    
+    int m1 = 33; 
+    int m2 = 66;
+    printEquation(m1, m2,sum(m1, m2),'+');
     return 0;
 }
+
+void printEquation(int m1, int m2, int sum, char operation) 
+{
+     //print the equation 'm1 operation  m2 = result'
+     cout<<m1<<operation<<m2<<"="<<sum;
+}
+
+int sum(int m1, int m2)
+{
+     return m1+m2;
+}
+
+
